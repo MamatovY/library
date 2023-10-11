16e96ca066782016c4b7fcb6f6fae8bafd64e883
MamatovY <mamatovyiman28@gmail.com>
2023-10-11 11:58:05 +0600

    Оптимизация кода

--- src/app/book/[id]/page.js
+++ src/app/book/[id]/page.js
@@ -5,7 +5,6 @@ import Image from 'next/image'
 export async function generateMetadata({ params }) {
     const book = await getBook(params.id)
 
-
     return {
         title: book.volumeInfo.title
     }
@@ -15,7 +14,7 @@ export async function generateMetadata({ params }) {
 const BookPage = async ({ params }) => {
     const book = await getBook(params.id)
     const img = book?.volumeInfo?.imageLinks?.thumbnail ? book?.volumeInfo?.imageLinks?.thumbnail : skeleton
-    console.log(img);
+
     return (
         <main className='container mt-10 gap-6'>
             <Image width={100} height={100} src={img} alt="" className="rounded-xl w-[16rem] md:w-[20rem] mx-auto sm:mx-0 sm:float-left sm:mr-5 mb-5" />
